75bfa7a5bb7371316c4140bb409a5c8666e404f5
hujuu <1961946+hujuu@users.noreply.github.com>
2024-06-28 10:34:29 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import Home from './pages/Home.vue'
-import About from './pages/About.vue'
-// import NotFound from './NotFound.vue'
-
-const routes = {
-  '/': Home,
-  '/about': About
-}
+import { ref } from 'vue'
 
 const currentPath = ref(window.location.hash)
 
 window.addEventListener('hashchange', () => {
   currentPath.value = window.location.hash
-})
-
-const currentView = computed(() => {
-  return routes[currentPath.value.slice(1) || '/'] || NotFound
 })
 </script>
 
