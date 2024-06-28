bcb12a71295f03c5c62de7af7b4d46d23dc61de6
hujuu <1961946+hujuu@users.noreply.github.com>
2024-06-28 13:12:06 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,12 @@ window.addEventListener('hashchange', () => {
 </script>
 
 <template>
-  <h1>Hello App!</h1>
   <p>
     <strong>Current route path:</strong> {{ $route.fullPath }}
   </p>
   <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/about">Go to About</RouterLink>
+    <RouterLink to="/">Home </RouterLink>
+    <RouterLink to="/about"> About</RouterLink>
   </nav>
   <main>
     <RouterView />
@@ -23,16 +22,5 @@ window.addEventListener('hashchange', () => {
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
